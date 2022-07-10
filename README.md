60d192c8717321125d5df75b9cad11bbbce96d57
Katelyn Baker <katelyn.baker@r3.com>
2022-07-10 23:40:40 +0100

    wip

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-mvn package
+# Corda Avro Schema Generator
 
-java -jar target/CordaAvro-1.0.jar
+## Purpose
+
+Simple exe that allows for the dumping to the command line of of the Corda AVRO Schema fingerprints. Why is this
+usful? easy, the C++ Avro code doesn't support fingerprinting yet
+
+## Build
+
+```mvn package```
+
+## Execute
+
+```java -jar target/CordaAvro-1.0.jar```
--- src/main/kotlin/net/fenryka/corda_avro/main.kt
+++ src/main/kotlin/net/fenryka/corda_avro/main.kt
@@ -3,15 +3,27 @@ package net.fenryka.corda_avro
 import org.apache.avro.Schema
 import org.apache.avro.SchemaNormalization
 
-fun main(){
-    println ("Hello world");
-    val schema = Schema.Parser().parse(
-        ClassLoader.getSystemClassLoader().getResourceAsStream("Envelope.avsc")
+fun schema (outer: String, files_: List<String> = listOf()) {
+    val parser = Schema.Parser();
+    files_.forEach { file ->
+        parser.parse(
+            ClassLoader.getSystemClassLoader().getResourceAsStream (file)
+        )
+    }
+
+    val schema = parser.parse(
+        ClassLoader.getSystemClassLoader().getResourceAsStream (outer)
     )
 
-    val fingerprint = SchemaNormalization.parsingFingerprint("SHA-256", schema);
-    println (schema);
-    println ("---")
-    println (SchemaNormalization.toParsingForm(schema))
-    println (fingerprint);
+
+    val fingerprint = SchemaNormalization.parsingFingerprint("SHA-256", schema)
+
+    println ("$outer::$fingerprint")
+}
+
+fun main(){
+    schema ("Envelope.avsc")
+    schema ("HoldingIdentity.avsc")
+    schema ("UnauthenticatedMessageHeader.avsc", listOf ("HoldingIdentity.avsc"))
+    schema ("UnauthenticatedMessage.avsc", listOf ("HoldingIdentity.avsc", "UnauthenticatedMessageHeader.avsc"))
 }
